8c52eb598db7f520911ebcdbf8131fa72bb394e8
Rafal <rf.zajac@gmail.com>
2019-04-30 22:33:39 +0200

    prepared modules for further work

--- web/src/main/java/org/rafalzajac/web/WebApplication.java
+++ web/src/main/java/org/rafalzajac/web/WebApplication.java
@@ -2,8 +2,10 @@ package org.rafalzajac.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.rafalzajac")
 public class WebApplication {
 
     public static void main(String[] args) {
