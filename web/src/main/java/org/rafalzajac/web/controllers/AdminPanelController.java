b1021ce7cb986e28ead211765ab96ee45155db58
Rafal <rf.zajac@gmail.com>
2019-05-31 13:59:04 +0200

    added views for administration, now need to correct views and controllers for viewing and admin

--- web/src/main/java/org/rafalzajac/web/controllers/AdminPanelController.java
+++ web/src/main/java/org/rafalzajac/web/controllers/AdminPanelController.java
@@ -1,21 +1,18 @@
 package org.rafalzajac.web.controllers;
 
 import org.rafalzajac.domain.Match;
-import org.rafalzajac.domain.MatchResult;
 import org.rafalzajac.domain.Round;
 import org.rafalzajac.domain.Team;
 import org.rafalzajac.service.*;
 import org.rafalzajac.web.administration.CreateNewElement;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminPanelController {
 
     private TeamService teamService;
@@ -32,9 +29,26 @@ public class AdminPanelController {
         this.teamStatsService = teamStatsService;
     }
 
-    @GetMapping("/admin")
+    @GetMapping("/")
     public String adminPanel() {
         return "administration/adminPanel";
+    }
+
+    @GetMapping("/round-admin")
+    public String league(Model model) {
+
+        List<Round> rounds = roundService.findAllRounds();
+        List<Match> matchList = matchService.findAllMatches();
+        model.addAttribute("rounds", rounds);
+
+        return "administration/views/roundAdmin";
+    }
+
+    @GetMapping("/teams-admin")
+    public  String teamData(Model model) {
+        List<Team> allTeams = teamService.findAllTeams();
+        model.addAttribute("teams", allTeams);
+        return "administration/views/teamsAdmin";
     }
 
     @GetMapping("/creatematch")
@@ -48,7 +62,7 @@ public class AdminPanelController {
         model.addAttribute("allTeams", teams);
 
 
-        return "administration/createMatch";
+        return "administration/createElements/createMatch";
     }
 
     @PostMapping("/creatematch")
@@ -64,7 +78,7 @@ public class AdminPanelController {
     @GetMapping("/createteam")
     public String createTeam(Model model, @ModelAttribute("newTeam") Team team) {
 
-        return "administration/createTeam";
+        return "administration/createElements/createTeam";
     }
 
     @PostMapping("/createteam")
