a6e4bf44503fe39d2206fceb178e894c321139fd
Rafal <rf.zajac@gmail.com>
2019-06-07 12:56:26 +0200

    deleted unnecessary dependencies in modules and styled to center all the inputs

--- domain/src/main/java/org/rafalzajac/domain/MatchResult.java
+++ domain/src/main/java/org/rafalzajac/domain/MatchResult.java
@@ -7,8 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+
 
 @Entity
 @NoArgsConstructor
