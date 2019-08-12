3b03e243c1fb0c61846bee04dccf7c22991605cd
Rafal <rf.zajac@gmail.com>
2019-08-12 13:47:49 +0200

    added test for News class and added few preconditions for its setter methods

--- domain/src/main/java/org/rafalzajac/domain/News.java
+++ domain/src/main/java/org/rafalzajac/domain/News.java
@@ -1,13 +1,15 @@
 package org.rafalzajac.domain;
 
-import lombok.Data;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import lombok.Getter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 
 @Entity
-@Data
+@Getter
 public class News {
 
     @Id
@@ -24,13 +26,34 @@ public class News {
     private LocalDateTime creationDate;
 
 
-    public News() {
+    /**
+     * No argument constructor used for factory method and also gets current time as creation date, and it is also
+     * saved as formatted string in createdAt variable
+     */
+    private News() {
         creationDate = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         createdAt = creationDate.format(formatter);
     }
 
+    /**
+     * Setter method for title.
+     * @param title - string value. Cannot be empty or null
+     */
+    public void setTitle(String title) {
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(title));
+        this.title = title;
+    }
+
+    /**
+     * Setter method for content. It checks input length. It cannot be null or empty and if it is shorter or equal to
+     * 100 characters then short description is the same as content. If content is longer then short description is set
+     * as first 100 chars of the content and "..." is added at the end.
+     * @param content - not empty, not null string value
+     */
     public void setContent(String content) {
+
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(content));
 
         if (content.length() > 100){
             shortDescription = content.substring(0,100);
@@ -40,5 +63,13 @@ public class News {
         }
 
         this.content = content;
+    }
+
+    /**
+     * Factory method for creating News object
+     * @return - News instance with default values
+     */
+    public static News create() {
+        return new News();
     }
 }
---
+++ domain/src/test/java/org/rafalzajac/domain/NewsTest.java
@@ -0,0 +1,56 @@
+package org.rafalzajac.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class NewsTest {
+
+    private News news;
+
+    @Before
+    public void before() {
+        news = News.create();
+    }
+
+    @Test
+    public void createTest() {
+        assertThat(news.getId()).isNull();
+        assertThat(news.getTitle()).isNull();
+        assertThat(news.getShortDescription()).isNull();
+        assertThat(news.getContent()).isNull();
+        assertThat(news.getCreatedAt()).isNotEmpty();
+        assertThat(news.getCreationDate()).isBefore(LocalDateTime.now());
+    }
+
+    @Test
+    public void setValidTitle() {
+        news.setContent("Valid content");
+        assertThat(news.getContent()).isEqualTo("Valid content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setEmptyContent() {
+        news.setContent("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setNullContent() {
+        news.setContent(null);
+    }
+
+    @Test
+    public void checkShortDescriptionShorterThan100Chars() {
+        news.setContent("Short content");
+        assertThat(news.getShortDescription().length()).isEqualTo(13);
+    }
+
+    @Test
+    public void checkShortDescriptionLongerThan100Chars() {
+        news.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua");
+        assertThat(news.getShortDescription().length()).isEqualTo(103);
+    }
+}
